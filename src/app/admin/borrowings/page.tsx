7918d2104b1a0c19920356ca82d3ba2e56b837c6
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2024-12-03 15:24:45 -0600

    feat(borrowings): add base structure for Borrowings page with UI and table components
        - Created TableRenderer and CrudBody components for Borrowings
        - Added initial Borrowings page with basic UI structure

--- src/app/admin/borrowings/page.tsx
+++ src/app/admin/borrowings/page.tsx
@@ -1,9 +1,65 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
+import CrudHeader from '@/components/AdminCrud/CrudHeader'
+import CrudBody from '@/components/AdminCrud/CrudBodyBorrowings'
+import fetchData from '@/services/fetchData'
+import endpoints from '@/app/infraestructure/config/configAPI'
+import { getToken } from '@/services/storageService'
 
 export default function page() {
+
+    const token = getToken()
+
+    interface borrow {
+        borrowId: number;
+        date: string;
+        startDate: string | null;
+        endDate: string | null;
+        returnDate: string | null;
+        status: string;
+        amount: number;
+        usuarioId: number;
+        usuarioName: string;
+        adminId: number;
+        adminName: string;
+        details: any;
+    }
+
+    const [data, setData] = useState<borrow[]>([])
+
+    const filterBorrowings = (borrowings: borrow[]) => {
+        return borrowings.filter(borrowing => borrowing.status === "BORROWED" || borrowing.status === "RETURNED");
+    }
+
+    const fetchBorrowings = () => {
+        fetchData(endpoints.borrowings.getAll, token)
+            .then((data) => {
+                console.log(data);
+                setData(filterBorrowings(data));
+            })
+    }
+
+    useEffect(() => {
+        fetchBorrowings()
+    }, [])
+
     return (
         <div>
-
+            <CrudHeader
+                dropdownOptions={['null']}
+                title='Préstamos'
+                buttonDisabled={true}
+                onSearchChange={() => { }}
+                buttonFunction={() => { }}
+                buttonLabel=''
+            />
+            <CrudBody
+                data={data}
+                headers={['borrowId', 'usuarioName', 'startDate', 'endDate', 'status']}
+                searchTerm=''
+                onMoreInfo={() => { }}
+                onConfirmReturn={() => { }}
+            />
         </div>
     )
 }
---
+++ src/components/AdminCrud/TableRendererBorrowings.tsx
@@ -0,0 +1,76 @@
+import { FaCircleInfo } from 'react-icons/fa6';
+import RollingSpinner from '@/assets/animations/rolling-spinner.svg';
+import { RiArrowTurnBackLine } from "react-icons/ri";
+
+interface TableRowsProps {
+    headers: string[];
+    currentRecords: any[];
+    onConfirmReturn: (id: number) => void;
+    onMoreInfo: (id: number) => void;
+}
+
+const TableRows: React.FC<TableRowsProps> = ({ headers, currentRecords, onConfirmReturn, onMoreInfo }) => {
+    // Si currentRecords está vacío, mostramos el spinner
+    if (currentRecords.length === 0) {
+        return (
+            <tr >
+                <td rowSpan={6} colSpan={6} className="text-center">
+                    <div className="flex justify-center items-center pt-6">
+                        <RollingSpinner width={80} height={80} />
+                    </div>
+                    <h1 className="flex justify-center items-center pt-6 font-semibold text-lg">
+                        Cargando contenido ...
+                    </h1>
+                </td>
+            </tr>
+        );
+    }
+
+    return (
+        <>
+            {currentRecords.map((row: any, rowIndex: number) => {
+                const idKey = Object.keys(row)[0];
+                const idValue = row[idKey];
+
+                return (
+                    <tr key={`row-${idValue}-${rowIndex}`}>
+                        {headers.map((header: string, headerIndex: number) => (
+                            <td key={`${header}-${idValue}-${headerIndex}`} onClick={() => onConfirmReturn(idValue)}>
+                                {row[header]}
+                            </td>
+                        ))}
+                        <td key={`actions-td-${idValue}`} className='flex justify-end space-x-4'>
+                            <button
+                                className="bg-primary text-white rounded-md w-8 h-8 flex items-center justify-center hover:bg-secondary"
+                                onClick={() => onConfirmReturn(idValue)}
+                            >
+                                <RiArrowTurnBackLine size={16} />
+                            </button>
+                            <button className='text-secondary w-8 h-8 flex items-center justify-center'
+                                onClick={() => onMoreInfo(idValue)}>
+                                <FaCircleInfo size={26} />
+                            </button>
+                        </td>
+                    </tr>
+                );
+            })}
+        </>
+    );
+};
+
+interface TableHeadersProps {
+    headers: string[];
+}
+
+const TableHeaders: React.FC<TableHeadersProps> = ({ headers }) => {
+    return (
+        <tr className="text-lg">
+            {headers.map((header: string, index: number) => (
+                <th key={`${header}-${index}`}>{header.toUpperCase()}</th>
+            ))}
+            <th key="actions-th"></th>
+        </tr>
+    );
+};
+
+export { TableRows, TableHeaders };
