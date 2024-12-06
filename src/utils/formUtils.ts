860b3e7b84b87038c14f6f268d70c8d4aa987ee3
RaulBecerraB <ingenierobecerrabarcelo@gmail.com>
2024-12-05 21:27:41 -0600

    chore: remove unused formUtils for cleaner code
        - Deleted formUtils as it was not in use and caused issues
        - Improved overall code quality by eliminating redundant files

--- src/utils/formUtils.ts
+++
@@ -1,7 +0,0 @@
-const togglePasswordVisibility = (type: any) => {
-    if (type === 'password') {
-        setPasswordVisible(!passwordVisible);
-    } else {
-        setConfirmPasswordVisible(!confirmPasswordVisible);
-    }
-};
