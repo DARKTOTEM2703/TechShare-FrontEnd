f546e7046a69d174206da0cf765c18c7324cf8c7
RaulBecerraB <ingenierobecerrabarcelo@gmail.com>
2025-01-21 13:44:00 -0600

    feat(subcategories): implement React Cropper for image editing in create and edit forms
        - Added React Cropper to the create and edit forms for subcategories
        - Enabled users to crop images before uploading
        - Ensured seamless integration with the existing form structure

--- src/components/AdminCrud/MaterialCatalog/SubCategories.tsx
+++ src/components/AdminCrud/MaterialCatalog/SubCategories.tsx
@@ -7,6 +7,8 @@ import { useCrudOperations } from '@/hooks/useCrudOperations';
 import endpoints from '@/app/infraestructure/config/configAPI';
 import DynamicDropdown from '@/components/Dropdowns/DynamicDropdown';
 import DropzoneWithPreview from '@/components/DropZone';
+import { useImageCrop } from '@/hooks/useReactCrop';
+import ReactCrop from 'react-image-crop';
 
 export default function SubCategories({ token, categories, subCategories, refreshSubCategories }: { token: any, categories: any, subCategories: any, refreshSubCategories: any }) {
 
@@ -14,25 +16,55 @@ export default function SubCategories({ token, categories, subCategories, refres
     const [isCreateModalVisible, setCreateModalVisible] = useState(false);
     const [isEditModalVisible, setEditModalVisible] = useState(false);
     const [isDeleteModalVisible, setDeleteModalVisible] = useState(false);
-    const [formData, setFormData] = useState<{ name: string; image?: File | null; idCategory: number }>({ name: '', image: null, idCategory: 0 });
+    const [formData, setFormData] = useState<{ name: string; image: File | null; imagePreview: string | null; idCategory: number }>({ name: '', image: null, imagePreview: null, idCategory: 0 });
 
     const [selectedSubCategory, setSelectedSubCategory] = useState<any>(null);
 
     const { setClickedItemId, handleCreate, handleUpdate, handleDelete, clickedItemId } = useCrudOperations(token, refreshSubCategories);
 
+    const ASPECT_RATIO = 1;
+    const MIN_DIMENSION = 100;
+    const MIN_WIDTH = 100;
+
+    const {
+        imageUrl,
+        setImageUrl,
+        crop,
+        applyCrop,
+        setCrop,
+        imageRef,
+        previewImageRef,
+        isImageCropping,
+        onSelectFile,
+        onImageLoad,
+        setIsImageCropping
+    } = useImageCrop({ ASPECT_RATIO, MIN_DIMENSION, MIN_WIDTH });
+
     const handleSearchChange = (value: string) => setSearchTerm(value);
 
     const showCreateModal = () => setCreateModalVisible(true);
-    const hideCreateModal = () => setCreateModalVisible(false);
+    const hideCreateModal = () => {
+        setCreateModalVisible(false);
+        setFormData({ name: '', image: null, imagePreview: null, idCategory: 0 });
+        setIsImageCropping(false);
+        setImageUrl('');
+        setCrop(undefined);
+    };
 
     const showEditModal = () => setEditModalVisible(true);
-    const hideEditModal = () => setEditModalVisible(false);
+    const hideEditModal = () => {
+        setEditModalVisible(false);
+        setFormData({ name: '', image: null, imagePreview: null, idCategory: 0 });
+        setIsImageCropping(false);
+        setImageUrl('');
+        setCrop(undefined);
+    };
 
     const showDeleteModal = () => setDeleteModalVisible(true);
     const hideDeleteModal = () => setDeleteModalVisible(false);
 
     const createButtonClicked = () => {
-        setFormData({ name: '', image: null, idCategory: 0 });
+        setFormData({ name: '', image: null, imagePreview: null, idCategory: 0 });
         showCreateModal();
     }
 
@@ -41,7 +73,7 @@ export default function SubCategories({ token, categories, subCategories, refres
         setSelectedSubCategory(selectedSubCategory);
         if (selectedSubCategory) {
             setClickedItemId(id);
-            setFormData({ name: selectedSubCategory.name, image: selectedSubCategory.imagePath, idCategory: selectedSubCategory.categoryId });
+            setFormData({ name: selectedSubCategory.name, image: null, imagePreview: selectedSubCategory.imagePath, idCategory: selectedSubCategory.categoryId });
             showEditModal();
         }
     };
@@ -77,7 +109,6 @@ export default function SubCategories({ token, categories, subCategories, refres
         hideEditModal();
     };
 
-
     const handleSubCategoryDeletion = (e: any) => {
         e.preventDefault();
         if (clickedItemId !== null) {
@@ -95,19 +126,77 @@ export default function SubCategories({ token, categories, subCategories, refres
                 <div className="modal-overlay">
                     <div className="modal-content">
                         <ModalBase onClose={hideCreateModal} header="Create New SubCategory" onSubmit={handleSubCategoryCreation}>
-                            <BorderTextField name="name"
-                                placeholder="SubCategory Name"
-                                onChange={(e) => setFormData({ ...formData, name: e.target.value })} value={formData.name} />
-                            <DynamicDropdown
-                                data={categories}
-                                valueKey="categoryId"
-                                labelKey="name"
-                                selectedValue={formData.idCategory}
-                                onChange={(value: number) => setFormData({ ...formData, idCategory: value })}
-                            />
-                            <DropzoneWithPreview
-                                onFileChange={(file) => setFormData({ ...formData, image: file })}
-                            />
+                            {isImageCropping ? (
+                                <>
+                                    <ReactCrop
+                                        crop={crop}
+                                        onChange={(_, percentCrop) => {
+                                            setCrop(percentCrop);
+                                        }}
+                                        keepSelection
+                                        aspect={ASPECT_RATIO}
+                                        minWidth={MIN_WIDTH}
+                                    >
+                                        <img
+                                            ref={imageRef}
+                                            src={imageUrl}
+                                            alt="Upload"
+                                            style={{ maxHeight: '70vh' }}
+                                            onLoad={onImageLoad}
+                                        />
+                                    </ReactCrop>
+
+                                    <canvas
+                                        ref={previewImageRef}
+                                        className="mt-4"
+                                        style={{
+                                            display: 'none',
+                                            border: '1px solid black',
+                                            objectFit: 'contain',
+                                            width: 150,
+                                            height: 150
+                                        }}
+                                    />
+
+                                    <button
+                                        className="primary-button"
+                                        type="button"
+                                        onClick={() => {
+                                            applyCrop((croppedFile: any, previewUrl: any) => {
+                                                setFormData((prev) => ({
+                                                    ...prev,
+                                                    image: croppedFile,
+                                                    imagePreview: previewUrl
+                                                }));
+                                            });
+                                        }}
+                                    >
+                                        Aplicar
+                                    </button>
+                                </>
+                            ) : (
+                                <>
+                                    <BorderTextField
+                                        name="name"
+                                        placeholder="SubCategory Name"
+                                        onChange={(e) =>
+                                            setFormData({ ...formData, name: e.target.value })
+                                        }
+                                        value={formData.name}
+                                    />
+                                    <DynamicDropdown
+                                        data={categories}
+                                        valueKey="categoryId"
+                                        labelKey="name"
+                                        selectedValue={formData.idCategory}
+                                        onChange={(value: number) => setFormData({ ...formData, idCategory: value })}
+                                    />
+                                    <DropzoneWithPreview
+                                        onFileChange={(file) => onSelectFile(file)}
+                                        initialPreview={formData.imagePreview || imageUrl}
+                                    />
+                                </>
+                            )}
                         </ModalBase>
                     </div>
                 </div>
@@ -117,25 +206,82 @@ export default function SubCategories({ token, categories, subCategories, refres
                 <div className="modal-overlay">
                     <div className="modal-content">
                         <ModalBase onClose={hideEditModal} header="Edit SubCategory" onSubmit={handleSubCategoryUpdate}>
-                            <BorderTextField name="name"
-                                placeholder="SubCategory Name"
-                                onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-                                value={formData.name} />
-                            <DynamicDropdown
-                                data={categories}
-                                valueKey="categoryId"
-                                labelKey="name"
-                                selectedValue={formData.idCategory}
-                                onChange={(value: number) => setFormData({ ...formData, idCategory: value })}
-                            />
-                            <DropzoneWithPreview
-                                onFileChange={(file) => setFormData({ ...formData, image: file })}
-                                initialPreview={selectedSubCategory.imagePath}
-                            />
+                            {isImageCropping ? (
+                                <>
+                                    <ReactCrop
+                                        crop={crop}
+                                        onChange={(_, percentCrop) => {
+                                            setCrop(percentCrop);
+                                        }}
+                                        keepSelection
+                                        aspect={ASPECT_RATIO}
+                                        minWidth={MIN_WIDTH}
+                                    >
+                                        <img
+                                            ref={imageRef}
+                                            src={imageUrl}
+                                            alt="Upload"
+                                            style={{ maxHeight: '70vh' }}
+                                            onLoad={onImageLoad}
+                                        />
+                                    </ReactCrop>
+
+                                    <canvas
+                                        ref={previewImageRef}
+                                        className="mt-4"
+                                        style={{
+                                            display: 'none',
+                                            border: '1px solid black',
+                                            objectFit: 'contain',
+                                            width: 150,
+                                            height: 150
+                                        }}
+                                    />
+
+                                    <button
+                                        className="primary-button"
+                                        type="button"
+                                        onClick={() => {
+                                            applyCrop((croppedFile: any, previewUrl: any) => {
+                                                setFormData((prev) => ({
+                                                    ...prev,
+                                                    image: croppedFile,
+                                                    imagePreview: previewUrl
+                                                }));
+                                            });
+                                        }}
+                                    >
+                                        Aplicar
+                                    </button>
+                                </>
+                            ) : (
+                                <>
+                                    <BorderTextField
+                                        name="name"
+                                        placeholder="SubCategory Name"
+                                        onChange={(e) =>
+                                            setFormData({ ...formData, name: e.target.value })
+                                        }
+                                        value={formData.name}
+                                    />
+                                    <DynamicDropdown
+                                        data={categories}
+                                        valueKey="categoryId"
+                                        labelKey="name"
+                                        selectedValue={formData.idCategory}
+                                        onChange={(value: number) => setFormData({ ...formData, idCategory: value })}
+                                    />
+                                    <DropzoneWithPreview
+                                        onFileChange={(file) => onSelectFile(file)}
+                                        initialPreview={formData.imagePreview || selectedSubCategory?.imagePath || ''}
+                                    />
+                                </>
+                            )}
                         </ModalBase>
                     </div>
                 </div>
             )}
+
             {isDeleteModalVisible && (
                 <div className="modal-overlay">
                     <div className="modal-content">
